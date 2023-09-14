3cef87470498eb563ed7dea25e92611bbaf45400
gavin <wswxg@hotmail.com>
2023-09-14 11:53:20 +0800

    clear sensitive file

--- system-service/src/test/java/com/atguigu/accounting/TestReadAndWrite.java
+++ system-service/src/test/java/com/atguigu/accounting/TestReadAndWrite.java
@@ -2,11 +2,17 @@ package com.atguigu.accounting;
 
 import com.atguigu.accounting.entity.Book;
 import com.atguigu.accounting.service.BookService;
+import org.apache.commons.lang3.StringUtils;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.EnvironmentStringPBEConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.List;
 
@@ -35,5 +41,21 @@ public class TestReadAndWrite {
     void test3(){
         List<Book> list = bookService.list();
         System.out.println(list);
+    }
+
+
+    //获取加密后的密文
+    @Autowired
+    private StringEncryptor encryptor;
+    @Test
+    void test4(){
+        List<String> properties = new ArrayList<>();
+        properties.add("");
+        properties.add("");
+        properties.add("");
+        properties.add("");
+        properties.forEach(property -> {
+            System.out.println(property + encryptor.encrypt(property));
+        });
     }
 }
